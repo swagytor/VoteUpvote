90e485a2824065e5b21cf68645b50b12f7acd734
hyozan <swagytor@yandex.ru>
2023-10-30 22:58:12 +0300

    Used SurveyViewSet instead Survey's Generics

--- survey/permissions.py
+++ survey/permissions.py
@@ -4,14 +4,20 @@ from rest_framework.permissions import BasePermission
 class IsSurveyOwner(BasePermission):
     """Является ли пользователем автором опроса"""
     def has_object_permission(self, request, view, obj):
-        user = request.user
+        if view.action in ['update', 'destroy']:
+            user = request.user
 
-        return obj.author == user
+            return obj.author == user
+
+        return True
 
 
 class IsQuestionOwner(BasePermission):
     """Является ли пользователем автором вопроса"""
     def has_object_permission(self, request, view, obj):
-        user = request.user
+        if view.action in ['update', 'destroy']:
+            user = request.user
 
-        return obj.survey.author == user
+            return obj.survey.author == user
+
+        return True
