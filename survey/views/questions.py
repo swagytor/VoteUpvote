c447c00385743c68c84a426b23e15660d5aa27be
hyozan <swagytor@yandex.ru>
2023-10-30 22:58:30 +0300

    Used QuestionViewSet instead of Question's Generics

--- survey/views/questions.py
+++ survey/views/questions.py
@@ -4,36 +4,50 @@ from rest_framework.permissions import IsAuthenticated
 from survey.models import Question
 from survey.permissions import IsQuestionOwner
 from survey.serializers import QuestionSerializer, CreateQuestionSerializer
+from survey.views.surveys import MultiSerializerViewSet
 
 
-class QuestionCreateAPIView(CreateAPIView):
-    """Контроллер для создания объекта Question"""
-    permission_classes = [IsAuthenticated]
-    serializer_class = CreateQuestionSerializer
-
-
-class QuestionListAPIView(ListAPIView):
-    """Контроллер для получения списка объектов Question"""
-    permission_classes = [IsAuthenticated]
-    serializer_class = QuestionSerializer
+class QuestionViewSet(MultiSerializerViewSet):
     queryset = Question.objects.all()
-
-
-class QuestionRetrieveAPIView(RetrieveAPIView):
-    """Контроллер для получения детальной информации объекта Question"""
-    permission_classes = [IsAuthenticated]
-    serializer_class = QuestionSerializer
-    queryset = Question.objects.all()
-
-
-class QuestionUpdateAPIView(UpdateAPIView):
-    """Контроллер для обновления объекта Question"""
     permission_classes = [IsAuthenticated, IsQuestionOwner]
-    serializer_class = QuestionSerializer
-    queryset = Question.objects.all()
+
+    serializers = {
+        'create': CreateQuestionSerializer,
+        'list': QuestionSerializer,
+        'retrieve': QuestionSerializer,
+        'update': QuestionSerializer,
+        'default': QuestionSerializer,
+    }
 
 
-class QuestionDestroyAPIView(DestroyAPIView):
-    """Контроллер для удаления объекта Question"""
-    permission_classes = [IsAuthenticated, IsQuestionOwner]
-    queryset = Question.objects.all()
+# class QuestionCreateAPIView(CreateAPIView):
+#     """Контроллер для создания объекта Question"""
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = CreateQuestionSerializer
+#
+#
+# class QuestionListAPIView(ListAPIView):
+#     """Контроллер для получения списка объектов Question"""
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = QuestionSerializer
+#     queryset = Question.objects.all()
+#
+#
+# class QuestionRetrieveAPIView(RetrieveAPIView):
+#     """Контроллер для получения детальной информации объекта Question"""
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = QuestionSerializer
+#     queryset = Question.objects.all()
+#
+#
+# class QuestionUpdateAPIView(UpdateAPIView):
+#     """Контроллер для обновления объекта Question"""
+#     permission_classes = [IsAuthenticated, IsQuestionOwner]
+#     serializer_class = QuestionSerializer
+#     queryset = Question.objects.all()
+#
+#
+# class QuestionDestroyAPIView(DestroyAPIView):
+#     """Контроллер для удаления объекта Question"""
+#     permission_classes = [IsAuthenticated, IsQuestionOwner]
+#     queryset = Question.objects.all()
