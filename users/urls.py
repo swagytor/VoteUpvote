77696dd31dfedd3cd1679f49a5f29cbddd760ab5
hyozan <swagytor@yandex.ru>
2023-10-30 22:57:01 +0300

    Removed Survey's views from users app

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import vk_authentication, FavoritesSurveyListAPIView, MySurveyListAPIView, SurveyHistoryListAPIView
+from users.views import vk_authentication
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('authentication/vk/', vk_authentication, name='auth-vk'),
-    path('favorites/', FavoritesSurveyListAPIView.as_view(), name='survey-favorite'),
-    path('my_surveys/', MySurveyListAPIView.as_view(), name='survey-user'),
-    path('history/', SurveyHistoryListAPIView.as_view(), name='survey-history')
 ]
